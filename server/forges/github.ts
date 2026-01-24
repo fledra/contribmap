58ecf4dfd909c8cded87b2a334ec5e3d021f5e94
Fledra <baran@fledra.dev>
2026-01-25 01:06:49 +0300

    added yearly chunking to github fetcher

--- server/forges/github.ts
+++ server/forges/github.ts
@@ -43,6 +43,28 @@ const query = `
   }
 `;
 
+function chunkQueries(options: ContributionFetcherOptions) {
+  const chunks: ContributionFetcherOptions[] = [];
+  const MS_PER_YEAR = (MS_PER_DAY * DAYS_PER_YEAR) + MS_PER_DAY; // current day inclusive
+
+  const to = +new Date(options.to);
+  let from = +new Date(options.from);
+
+  while (from < to) {
+    const currentTo = Math.min(from + MS_PER_YEAR, to);
+
+    chunks.push({
+      ...options,
+      from: new Date(from).toISOString(),
+      to: new Date(currentTo).toISOString(),
+    });
+
+    from = currentTo;
+  }
+
+  return chunks;
+}
+
 export const fetchGithub: ContributionFetcher = async (config, options) => {
   let token: string | undefined;
 
@@ -50,31 +72,38 @@ export const fetchGithub: ContributionFetcher = async (config, options) => {
     token = getToken(config.token);
   }
 
-  const response = await $fetch<GithubResponse>(BASE_URL, {
-    method: 'POST',
-    headers: {
-      ...(token && { Authorization: `Bearer ${token}` }),
-    },
-    body: {
-      query,
-      variables: {
-        username: config.username,
-        from: new Date(options.from).toISOString(),
-        to: new Date(options.to).toISOString(),
+  const chunks = chunkQueries(options);
+  const queries = await Promise.all(
+    chunks.map((chunk) => $fetch<GithubResponse>(BASE_URL, {
+      method: 'POST',
+      headers: {
+        ...(token && { Authorization: `Bearer ${token}` }),
       },
-    },
-  });
+      body: {
+        query,
+        variables: {
+          username: config.username,
+          from: new Date(chunk.from).toISOString(),
+          to: new Date(chunk.to).toISOString(),
+        },
+      },
+    })),
+  );
 
-  const contributions = response
-    .data
-    .user
-    .contributionsCollection
-    .contributionCalendar
-    .weeks
-    .flatMap((week) => week.contributionDays.map<Contribution>((day) => ({
-      date: getISODate(day.date),
-      count: day.contributionCount,
-    })));
+  const contributions = queries.reduce<Contribution[]>((acc, res) => {
+    const weeks = res.data.user.contributionsCollection.contributionCalendar.weeks;
+
+    for (const week of weeks) {
+      for (const day of week.contributionDays) {
+        acc.push({
+          date: getISODate(day.date),
+          count: day.contributionCount,
+        });
+      }
+    }
+
+    return acc;
+  }, []);
 
   return contributions;
 };
